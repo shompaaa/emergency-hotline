9cf2d4d4b87fbad7214927f077b83122e4c35f68
shompaaa <shompaakter334@gmail.com>
2026-06-07 12:05:16 +0600

    copy text count and dynamic subtitle when calling done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Heart Counter
 
+const callHistoryData = [];
+
 const counts = document.querySelectorAll(".heart");
 for (const count of counts) {
   count.addEventListener("click", function () {
@@ -17,11 +19,27 @@ for (const call of emergencyCall) {
     const coinCountElement = document.getElementById("coin");
     const coinCount = parseInt(coinCountElement.innerText);
     if (coinCount < 20) {
-      alert("Insufficient balance to make a call 🥲");
+      alert("❌ আপনার পর্যাপ্ত কয়েন নেই। কল করতে কমপক্ষে ২০ কয়েন লাগবে!");
       return
     }
     let finalCoinCount = coinCount - 20;
     coinCountElement.innerText = finalCoinCount;
-    alert("Calling ........");
+
+
+    const number = this.dataset.number
+    const card = this.closest('.p-8')
+    const englishTitle = card.querySelector('.sub-title').innerText
+    alert(`Calling ${englishTitle} ${number} ........`);
   });
+}
+
+//Copy Text Count
+const copyCounts = document.querySelectorAll('.copy')
+for(copy of copyCounts){
+    copy.addEventListener('click',function(){
+        const copyCountElement = document.getElementById('copy-count')
+        const copyCount = parseInt(copyCountElement.innerText)
+        const finalCopyCount = copyCount + 1
+        copyCountElement.innerText = finalCopyCount
+    })
 }
